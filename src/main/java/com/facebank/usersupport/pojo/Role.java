4add0f17b595fa47e87dd163070c635ab7cf459f
KrishnnaOUT <zhangningkui@163.com>
2018-03-08 13:24:16 +0800

    system with static source and sql file

---
+++ src/main/java/com/facebank/usersupport/pojo/Role.java
@@ -0,0 +1,73 @@
+package com.facebank.usersupport.pojo;
+
+public class Role {
+    private Long roleId;
+
+    private String roleName;
+
+    private Short status;
+
+    private Long gmtCreate;
+
+    private Long gmtModify;
+
+    private String creator;
+
+    private String description;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public Short getStatus() {
+        return status;
+    }
+
+    public void setStatus(Short status) {
+        this.status = status;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModify() {
+        return gmtModify;
+    }
+
+    public void setGmtModify(Long gmtModify) {
+        this.gmtModify = gmtModify;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
