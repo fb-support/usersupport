fffcf4870b0fab5ccae2adb22148871c9f58e577
hubiao <1033718051@qq.com>
2018-03-12 10:59:53 +0800

    还款查询业务初步完成

---
+++ src/main/java/com/facebank/usersupport/dto/reqDto/RepaymentForm.java
@@ -0,0 +1,50 @@
+package com.facebank.usersupport.dto.reqDto;
+
+/**
+ * serviceDto
+ * serviceController 接受前端表单的数据。
+ * @author HuBiao
+ * @date 2018/3/9 000912:53
+ **/
+public class RepaymentForm {
+
+    private Long userId;
+
+    private Long orderId;
+
+    private Long startTime;
+
+    private Long endTime;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public Long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Long endTime) {
+        this.endTime = endTime;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+}
