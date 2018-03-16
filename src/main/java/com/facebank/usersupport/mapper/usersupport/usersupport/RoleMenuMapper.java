93bf9024a26abd71bb396546ed319b8bac48cbb9
KrishnnaOUT <zhangningkui@163.com>
2018-03-16 10:30:39 +0800

    规范了RoleService类中的编译不通过bug。

--- src/main/java/com/facebank/usersupport/mapper/usersupport/usersupport/RoleMenuMapper.java
+++ src/main/java/com/facebank/usersupport/mapper/usersupport/usersupport/RoleMenuMapper.java
@@ -2,9 +2,11 @@ package com.facebank.usersupport.mapper.usersupport.usersupport;
 
 import com.facebank.usersupport.model.RoleMenuModel;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RoleMenuMapper {
     int deleteByPrimaryKey(Long id);
 
