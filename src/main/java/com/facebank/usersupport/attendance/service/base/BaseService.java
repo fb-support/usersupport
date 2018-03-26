876b4608f4c93985df19278fe9fc049b3e3c50a4
KrishnnaOUT <zhangningkui@163.com>
2018-03-26 10:24:07 +0800

    增加了考勤模块、提测模块子目录

---
+++ src/main/java/com/facebank/usersupport/attendance/service/base/BaseService.java
@@ -0,0 +1,12 @@
+package com.facebank.usersupport.attendance.service.base;
+
+/**
+ * baseServcie
+ *
+ * @author hailong.Yang
+ * @create 2018-03-05 下午6:02
+ **/
+
+public abstract class BaseService {
+
+}
---
+++ src/main/java/com/facebank/usersupport/online/process/service/base/BaseService.java
@@ -0,0 +1,12 @@
+package com.facebank.usersupport.online.process.service.base;
+
+/**
+ * baseServcie
+ *
+ * @author hailong.Yang
+ * @create 2018-03-05 下午6:02
+ **/
+
+public abstract class BaseService {
+
+}
