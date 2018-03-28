f7f173bc47a2b2cbff25f9c7e78c022d7634f769
yaozun <2812575654@qq.com>
2018-03-28 09:16:12 +0800

    资金记录修改

--- src/main/java/com/facebank/usersupport/mapper/usersupport/p2p/MoneyRecordMapper.java
+++ src/main/java/com/facebank/usersupport/mapper/usersupport/p2p/MoneyRecordMapper.java
@@ -2,10 +2,10 @@ package com.facebank.usersupport.mapper.usersupport.p2p;
 
 
 import com.facebank.usersupport.dto.CapitalDto;
-import org.apache.ibatis.annotations.Param;
 import com.facebank.usersupport.model.MoneyRecord;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.Date;
 import java.util.List;
 
@@ -31,5 +31,7 @@ public interface MoneyRecordMapper {
 
 
     Integer getPageCount(@Param("mobile") String mobile, @Param("type") String type, @Param("starttime") Date starttime, @Param("endtime") Date endtime);
+
+    List<MoneyRecord> selectByMobile(@Param("mobile") String mobile, @Param("type") String type, @Param("starttime") Date starttime, @Param("endtime") Date endtime);
 
 }
